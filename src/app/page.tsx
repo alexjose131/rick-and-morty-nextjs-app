c1bb7231d65ddcfeb766e870690706055a6bf1ac
alexjose131 <alfernandez@brenntagla.com>
2024-07-23 06:54:36 -0400

    applying page redirection to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import { useEffect } from "react";
+import { permanentRedirect } from "next/navigation";
+
 export default function Home() {
+  useEffect(() => {
+    // Redireccionar a la página de login
+    permanentRedirect("/auth/login");
+  }, []);
   return (
     <main className="">
       <p>Home works</p>
