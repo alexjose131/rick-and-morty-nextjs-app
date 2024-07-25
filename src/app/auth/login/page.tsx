062b3a3a265bd71ca6af9e5636fcf82f2ff380f0
alexjose131 <alfernandez@brenntagla.com>
2024-07-25 12:01:33 -0400

    added image logo

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -7,6 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { credentialsSchema } from "@/schemas/credentialsSchema";
 import { Input } from "@/components/ui/input";
 import { CredentialsInputs } from "@/types/app-types";
+import Image from "next/image";
 
 export default function LoginPage() {
   const {
@@ -25,29 +26,34 @@ export default function LoginPage() {
   };
 
   return (
-    <>
-      <section className="flex flex-col items-center justify-center w-full min-h-screen">
-        <form
-          className="flex flex-col gap-3 lg:w-2/5"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <label htmlFor="user" className="text-sm text-gray-300">
-            Usuario
-          </label>
-          <Input type="text" id="user" {...register("user")} />
-          <p className="text-sm text-accent">{errors.user?.message}</p>
+    <section className="flex flex-col items-center justify-center w-full min-h-screen">
+      <Image
+        width={300}
+        height={300}
+        className="pb-16"
+        src={"/rick_and_morty.png"}
+        alt="Login image brand for the Rick And Morty web app"
+      ></Image>
+      <form
+        className="flex flex-col gap-3 lg:w-1/6"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label htmlFor="user" className="text-sm text-gray-300">
+          Usuario
+        </label>
+        <Input type="text" id="user" {...register("user")} />
+        <p className="text-sm text-accent">{errors.user?.message}</p>
 
-          <label htmlFor="user" className="text-sm text-gray-300">
-            Contraseña
-          </label>
-          <Input type="password" id="user" {...register("password")} />
-          <p className="text-sm text-accent">{errors.password?.message}</p>
+        <label htmlFor="user" className="text-sm text-gray-300">
+          Contraseña
+        </label>
+        <Input type="password" id="user" {...register("password")} />
+        <p className="text-sm text-accent">{errors.password?.message}</p>
 
-          <Button type="submit" className="mt-5">
-            Iniciar sesión
-          </Button>
-        </form>
-      </section>
-    </>
+        <Button type="submit" className="mt-5">
+          Iniciar sesión
+        </Button>
+      </form>
+    </section>
   );
 }
